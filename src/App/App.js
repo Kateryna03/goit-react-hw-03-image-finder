0859e07a4c350ce97df258ecd74da7724b11039f
Kateryna Gurienkova <gurienkova@gmail.com>
2021-09-03 00:27:44 +0300

    loadMore, error

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,14 @@
 import React, { Component, Fragment } from 'react';
 //import shortid from 'shortid';
-import './App.css';
+//import './App.css';
 import * as Api from '../services/api';
 import Searchbar from '../components/Searchbar';
 //import Modal from '../components/Modal';
 import ImageGallery from '../components/ImageGallery';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import ErrorMessage from '../components/ErrorMessage/ErrorMessage';
+import Button from '../components/Button/Button';
+import LoaderComp from '../components/Loader/Loader';
 
 const Status = {
   IDLE: 'idle',
@@ -23,32 +25,84 @@ class App extends Component {
     images: [],
     isLoading: false,
     status: Status.IDLE,
+    error: null,
   };
 
   componentDidUpdate(prevProps, prevState) {
     const prev = prevState.request;
     const next = this.state.request;
 
+    if (this.state.page >= 2) {
+      //console.log(this.state.page);
+      this.scrollOnLoadButton();
+    }
+
     if (prev !== next) {
-      this.setState({ status: Status.PENDING });
-      const { request, page } = this.state;
-      Api.fetchImages(request, page)
-        .then(({ data }) =>
-          this.setState(prevState => ({
-            images: [...prevState.images, ...data.hits],
-            page: prevState.page + 1,
-            status: Status.RESOLVED,
-          })),
-        )
-        .catch(error => this.setState({ error, status: Status.REJECTED }));
+      this.pageRender();
+      // this.setState({ status: Status.PENDING });
+      // const { request, page } = this.state;
+      // Api.fetchImages(request, page)
+      //   .then(({ data }) =>
+      //     this.setState(prevState => ({
+      //       images: [...prevState.images, ...data.hits],
+      //       page: prevState.page + 1,
+      //       status: Status.RESOLVED,
+      //     })),
+
+      // )
+      //  .catch(error => this.setState({ error, status: Status.REJECTED }));
     }
   }
-  //Переношу в Гэллери айтем
-  // toggleModal = () => {
-  //   this.setState(({ showModal }) => ({
-  //     showModal: !showModal,
-  //   }));
-  // };
+
+  //плавная прокрутка изображений
+  scrollOnLoadButton = () => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: 'smooth',
+    });
+  };
+
+  pageRender = () => {
+    const { request, page } = this.state;
+    this.setState({ status: Status.PENDING });
+    Api.fetchImages(request, page)
+      .then(data => {
+        console.log(data);
+        if (page === 1) {
+          if (data.hits.length === 0) {
+            toast.error('No result were found for your search', {
+              theme: 'colored',
+              position: 'top-left',
+              autoClose: 5000,
+            });
+            return this.setState({ status: Status.IDLE });
+          }
+          this.setState({
+            images: data.hits,
+            status: Status.RESOLVED,
+          });
+        } else {
+          this.setState(prevState => ({
+            images: [...prevState.images, ...data.hits],
+            status: Status.RESOLVED,
+          }));
+        }
+      })
+
+      // .then(({ data }) => {
+      //   if (data.hits.length === 0) {
+      //     //console.log('')
+      //     this.setState({status:Status.IDLE})
+      //   };
+      //   this.setState(prevState => ({
+      //     images: [...prevState.images, ...data.hits],
+      //     page: prevState.page + 1,
+      //     status: Status.RESOLVED,
+      //   }))
+      // }
+      // )
+      .catch(error => this.setState({ error, status: Status.REJECTED }));
+  };
 
   handleFormSubmit = newRequest => {
     this.setState({ request: newRequest, page: 1, images: [] });
@@ -79,11 +133,19 @@ class App extends Component {
     }
 
     if (status === Status.PENDING) {
-      return <div>Loading...</div>;
+      return <LoaderComp></LoaderComp>;
     }
+
     if (status === Status.REJECTED) {
-      return <ErrorMessage message={error.message} />;
+      //console.log(error);
+      return <ErrorMessage message={error} />;
+
+      // (
+      // <Fragment>
+      //   <ErrorMessage message={error.message} />;
+      // </Fragment>)
     }
+
     if (status === Status.RESOLVED) {
       return (
         <Fragment>
@@ -105,6 +167,7 @@ class App extends Component {
           {/* <button type="button" onClick={this.toggleModal}>
             open/close
           </button> */}
+          <Button OnClick={this.pageRender}></Button>
         </Fragment>
       );
     }
--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,13 +1,10 @@
-import errorImage from './232.jpg';
+import errorImage from '../ErrorMessage/232.jpg';
 
 export default function ErrorMessage({ message }) {
   return (
     <div role="alert">
       <img src={errorImage} width="240" alt="error" />
-      <p>
-        Something went wrong...
-        <span>Error:{message}</span>
-      </p>
+      <p>Error:{message}ОШИБКА</p>
     </div>
   );
 }
